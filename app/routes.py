c529a5556eaccc708eae70ee6784a307d8029574
Quoc Duong PHUNG <qdphung19@gmail.com.com>
2022-07-07 23:02:30 +0200

    alpha 07/07/22

--- app/routes.py
+++ app/routes.py
@@ -9,35 +9,61 @@ from flask import request
 from werkzeug.urls import url_parse
 from app import db
 from app.forms import RegistrationForm
+from datetime import datetime
+from app.forms import EditProfileForm
+from app.forms import PostForm
+from app.models import Post
  
-@app.route('/')
-@app.route('/index')
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+ 
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Votre post est maintenant en ligne')
+        return redirect(url_for('index'))
     # user = {'username': 'Duong'}
-    posts = [
-        {
-            'author': {'username': 'Miguel Grinberg'},
-            'body': 'Flask Web Development: Developing Web Applications With Python'
-        },
-        {
-            'author': {'username': 'Robert Martin'},
-            'body': 'Clean Code: A Handbook of Agile Software Craftsmanship'
-        },
-        {
-            'author': {'username': 'Jason Myers'},
-            'body': 'Essential SQLAlchemy'
-        },
-        {
-            'author': {'username': 'Regina Obe'},
-            'body': 'PostgreSQL: Up & Running: A Practical Guide to the Advanced Open Source Database'
-        },
-        {
-            'author': {'username': 'Steve Krug'},
-            'body': 'Dont Make Me Think, Revisited: A Common Sense Approach to Web Usability'
-        }
-    ]
-    return render_template('index.html', title='Home', posts=posts)
+    # posts = [
+    #     {
+    #         'author': {'username': 'Miguel Grinberg'},
+    #         'body': 'Flask Web Development: Developing Web Applications With Python'
+    #     },
+    #     {
+    #         'author': {'username': 'Robert Martin'},
+    #         'body': 'Clean Code: A Handbook of Agile Software Craftsmanship'
+    #     },
+    #     {
+    #         'author': {'username': 'Jason Myers'},
+    #         'body': 'Essential SQLAlchemy'
+    #     },
+    #     {
+    #         'author': {'username': 'Regina Obe'},
+    #         'body': 'PostgreSQL: Up & Running: A Practical Guide to the Advanced Open Source Database'
+    #     },
+    #     {
+    #         'author': {'username': 'Steve Krug'},
+    #         'body': 'Dont Make Me Think, Revisited: A Common Sense Approach to Web Usability'
+    #     }
+    # ]
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Home', form=form,
+                           posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -47,10 +73,10 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
+            flash('Nom utilisateur ou mot de passe invalide')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
+        next_page = request.args.get('suivant')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
@@ -71,6 +97,78 @@ def register():
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('Congratulations, you are now a registered user!')
+        flash('Félicitations, vous êtes maintenant un utilisateur sur mon blog')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Enregistré!')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='modifier votre Profil', form=form)
+
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot follow yourself!')
+        return redirect(url_for('user', username=username))
+    current_user.follow(user)
+    db.session.commit()
+    flash('You are following {}!'.format(username))
+    return redirect(url_for('user', username=username))
+ 
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot unfollow yourself!')
+        return redirect(url_for('user', username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash('You are not following {}.'.format(username))
+    return redirect(url_for('user', username=username))
+
+@app.route('/explore')
+@login_required
+def explore():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template("index.html", title='Explore', posts=posts.items,
+                          next_url=next_url, prev_url=prev_url)
